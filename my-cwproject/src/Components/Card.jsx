6c0fa0ac96c684ea79cb8dc49e92a746140a36d0
Aman Dwivedi <amandwivedi71124.com>
2022-11-10 23:31:51 +0530

    added Day 2

--- my-cwproject/src/Components/Card.jsx
+++ my-cwproject/src/Components/Card.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import {Box,Flex,Input,Image,Button,Text,Select} from "@chakra-ui/react"
+import {Box,Flex,Input,Image,Button,Text,Select, color} from "@chakra-ui/react"
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import TabP from './TabP'
 const Card = () => {
   return (
-    <div>
-      <Box bg='red' w='80%' p="12%" h="80%" color='black' ml="10%" borderColor="red">
+    <div  style={{height:"25%"}}>
+      <div style={{backgroundColor:"white",width:"80%", height: "250px",padding:"12%" ,color:"black",marginLeft:"10%"}}>
+      <Box >
          
-      <Tabs  isFitted variant='enclosed' w="120%" mt="-20%" ml="-10%" >
+      <Tabs  isFitted variant='enclosed' w="120%" mt="-20%" ml="-10%">
   <TabList  mb='1em'>
     <Tab fontSize={20} fontWeight="bold">One</Tab>
     <Tab fontSize={20} fontWeight="bold">Two</Tab>
@@ -27,9 +28,19 @@ const Card = () => {
     <TabPanel>
     <TabP/>
     </TabPanel>
+    <TabPanel>
+      <TabP/>
+    </TabPanel>
+    <TabPanel>
+    <TabP/>
+    </TabPanel>
+    <TabPanel>
+    <TabP/>
+    </TabPanel>
   </TabPanels>
 </Tabs>
        </Box>
+       </div>
     </div>
   )
 }
